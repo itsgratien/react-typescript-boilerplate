6cc657a231d20b12520364d7e88a15e8fe220fbf
gratien <gracian2020@gmail.com>
2022-08-16 11:09:40 +0200

    added images into assets dir

--- src/components/Pages/Landing/Landing.tsx
+++ src/components/Pages/Landing/Landing.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import style from './Landing.module.scss';
 import classname from 'classnames';
+import { handIcon } from 'src/assets/images';
 
 const Landing = () => {
   return (
-    <div className={classname('relative', style.landing)}>
-      <h1>Landing</h1>
+    <div className={classname('relative flex items-center justify-center flex-col pt-10', style.landing)}>
+      <img src={handIcon} />
+      <div className='mt-10 font-bold'>Welcome to landing page</div>
     </div>
   );
 };
